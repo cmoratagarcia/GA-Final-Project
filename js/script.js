7cab3ea97c7cc6ba5f2d063853d33e22289a2bcd
cmoratagarcia <cmoratagarcia@gmail.com>
2022-04-11 15:11:39 -0700

    Added 2 beer pages, mobile accordion and sign up

--- js/script.js
+++ js/script.js
@@ -1,63 +1,96 @@
 /*Dropdown Menus.
 Show menu 1 (Beers) and hide the other 2*/
-document.getElementById("beer-dropdown").addEventListener("mouseover", function() {
-    document.getElementById("beer-menu").classList.add("show");
-    document.getElementById("breweries-menu").classList.remove("show");
-    document.getElementById("sign-in-menu").classList.remove("show");
+
+let beerDropdown = document.getElementById("beer-dropdown");
+let breweriesDropdown = document.getElementById("breweries-dropdown");
+let signInDropdown = document.getElementById("sign-in-dropdown");
+
+let beerMenu = document.getElementById("beer-menu");
+let breweriesMenu = document.getElementById("breweries-menu");
+let signInMenu = document.getElementById("sign-in-menu");
+
+
+beerDropdown.addEventListener("mouseover", function() {
+    beerMenu.classList.add("show");
+    breweriesMenu.classList.remove("show");
+    signInMenu.classList.remove("show");
 })
 
 /*Hide the menu when not hovering over the navigation*/
-document.getElementById("beer-dropdown").addEventListener("mouseleave", function() {
-    document.getElementById("beer-menu").classList.remove("show");
+beerDropdown.addEventListener("mouseleave", function() {
+    beerMenu.classList.remove("show");
 })
 
 /*Show menu 2 (Breweries) and hide the other 2*/
-document.getElementById("breweries-dropdown").addEventListener("mouseover", function() {
-    document.getElementById("beer-menu").classList.remove("show");
-    document.getElementById("breweries-menu").classList.add("show");
-    document.getElementById("sign-in-menu").classList.remove("show");
+breweriesDropdown.addEventListener("mouseover", function() {
+    beerMenu.classList.remove("show");
+    breweriesMenu.classList.add("show");
+    signInMenu.classList.remove("show");
 })
 
 /*Hide the menu when not hovering over the navigation*/
-document.getElementById("breweries-dropdown").addEventListener("mouseleave", function() {
-    document.getElementById("breweries-menu").classList.remove("show");
+breweriesDropdown.addEventListener("mouseleave", function() {
+    breweriesMenu.classList.remove("show");
 })
 
 /*Show menu 3 (Sign In) and hide the other 2*/
-document.getElementById("sign-in-dropdown").addEventListener("mouseover", function() {
-    document.getElementById("beer-menu").classList.remove("show");
-    document.getElementById("breweries-menu").classList.remove("show");
-    document.getElementById("sign-in-menu").classList.add("show");
+signInDropdown.addEventListener("mouseover", function() {
+    beerMenu.classList.remove("show");
+    breweriesMenu.classList.remove("show");
+    signInMenu.classList.add("show");
 })
 
 /*Hide the menu when not hovering over the navigation*/
-document.getElementById("sign-in-dropdown").addEventListener("mouseleave", function() {
-    document.getElementById("sign-in-menu").classList.remove("show");
+signInDropdown.addEventListener("mouseleave", function() {
+    signInMenu.classList.remove("show");
 })
 
 /*Mobile side-menu*/
 /* When you click on the hamburger icon, it shows the menu */
 document.getElementById("hamburger-menu").addEventListener("click", function() {
-    document.getElementById("side-menu").classList.add("show-menu");
+    document.getElementById("side-menu").classList.add("mobile-show-menu");
 })
 
 /* When you click the X, it hides the menu*/
 document.getElementById("menu-close").addEventListener("click", function() {
-    document.getElementById("side-menu").classList.remove("show-menu");
+    document.getElementById("side-menu").classList.remove("mobile-show-menu");
 })
 
-/*Save button. When you click on the star button, it toggles between Save/Saved. 
-This is the best option I have come up with so far. It changes to Saved but doesn't change back.*/
+/*Save button. When you click on the star button, it toggles between Save/Saved.*/
 
 
 let saveButton = document.querySelectorAll(".save-button");
 
 saveButton.forEach(item => {
-    item.addEventListener("click", function() {
-        if (saveButton.innerHTML == "<i class='far fa-regular fa-star'></i> Save") {
-            item.innerHTML = "<i class='fa fa-solid fa-star'></i> Saved";
+    item.addEventListener("click", function(e) {
+        let clickedItem = e.currentTarget;
+        let text = clickedItem.innerText.trim();
+        if (text == "Save") {
+            clickedItem.innerHTML = "<i class='fa fa-solid fa-star'></i> Saved";
         } else {
-            saveButton.innerHTML = "<i class='far fa-regular fa-star'></i> Save";
+            clickedItem.innerHTML = "<i class='far fa-regular fa-star'></i> Save";
         };
     })
+})
+
+/*Mobile dropdowns.
+Show menu 1 (Beers) and hide the other 2*/
+document.getElementById("mobile-beer-dropdown").addEventListener("click", function() {
+    document.getElementById("beer-item").classList.toggle("hidden");
+    document.getElementById("brewery-item").classList.add("hidden");
+    document.getElementById("sign-in-item").classList.add("hidden");
+})
+
+/*Show menu 2 (Breweries) and hide the other 2*/
+document.getElementById("mobile-breweries-dropdown").addEventListener("click", function() {
+    document.getElementById("beer-item").classList.add("hidden");
+    document.getElementById("brewery-item").classList.toggle("hidden");
+    document.getElementById("sign-in-item").classList.add("hidden");
+})
+
+/*Show menu 3 (Sign In) and hide the other 2*/
+document.getElementById("mobile-sign-in-dropdown").addEventListener("click", function() {
+    document.getElementById("beer-item").classList.add("hidden");
+    document.getElementById("brewery-item").classList.add("hidden");
+    document.getElementById("sign-in-item").classList.toggle("hidden");
 })
